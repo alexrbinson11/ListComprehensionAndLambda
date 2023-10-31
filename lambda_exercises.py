ca102ba3276168dd24b13736f2a300a14ffb6d9a
Alex Robinson <alex_robinson2@baylor.edu>
2023-10-30 23:34:51 -0500

    commit1

--- lambda_exercises.py
+++ lambda_exercises.py
@@ -8,8 +8,12 @@ Odd numbers from the said list:
 [1, 3, 5, 7, 9]
 '''
 
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+filtered_list = list(filter(lambda num: (num % 2 == 0), number_list))
+print(filtered_list)
 
-
+filtered_list = list(filter(lambda num: (num % 2 == 1), number_list))
+print(filtered_list)
 
 
 ''' 2)
@@ -18,8 +22,8 @@ find which days of the week have exactly 6 characters.
 
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-
-
+days = list(filter(lambda x: len(x)==6, weekdays))
+print(days)
 
 
 
@@ -39,7 +43,15 @@ After removing the specified words from the said list:
 
 '''
 
+color_list = ['orange', 'red', 'green', 'blue', 'white', 'black']
+#update_list = list(filter(lambda color: color != 'orange' and color != 'black', color_list))
+#print(update_list)
 
+#or
+
+remove = ['orange', 'black']
+update_list = list(filter(lambda color: color not in remove, color_list))
+print(update_list)
 
 
 
@@ -58,7 +70,10 @@ Remove all elements from 'list1' present in 'list2:
 [1, 3, 5, 7, 9, 10]
  '''
 
-
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2 = [2, 4, 6, 8]
+update_list = list(filter(lambda x: x not in list2, list1))
+print(update_list)
 
 
 
@@ -77,7 +92,11 @@ Elements of the said list that contain specific substring:
 []
 
 '''
-
+list1 = ['red', 'black', 'white', 'green', 'orange']
+search1 = list(filter(lambda x: 'ack' in x, list1))
+print(search1)
+search2 = list(filter(lambda x: 'abc' in x, list1))
+print(search2)
 
 
 
@@ -88,11 +107,18 @@ check whether a given string contains a capital letter, a lower case letter, a n
 (This is like a password verification function, HINT: Python function 'any' may be useful)
 '''
 
-str1 = "Hello8world"
+#str1 = "Hello8world"
 str1 = "HELLO"
-str1= "hello"
+#str1= "hello"
 
-
+check = lambda x: any([y.isupper() for y in x] and
+                       [y.islower() for y in x] and
+                       [y.isdigit() for y in x] and
+                       [len(x)>7])
+if check(str1):
+    print("pass")
+else:
+    print("fail")
 
 
 
@@ -104,11 +130,14 @@ str1= "hello"
 
 
 ''' 7)
-Write a Python program to sort a list of tuples using Lambda.
+Write a Python program to sort a list of tuples using Lambda.      
 
 # Original list of tuples:
 original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
 
-# Expected Result:
+# Expected Result: lambda + sort
 # [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
 '''
+original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+scores = sorted(original_scores, key = lambda x: x[1])
+print(scores)
--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -11,9 +11,11 @@ The list comprehension always returns a result list. '''
 
 
 '''
+original list = [x,y,z,...]
 new_list = []
 for i in original_list:
     if filter(i):
+        result = expression(i)
         new_list.append(expressions(i))  '''
 
 #You can obtain the same thing using list comprehension:
@@ -30,6 +32,61 @@ for i in original_list:
 
 #The filter part answers the question if the item should be transformed.
 
+x = [i for i in range (10)]
+print(x)
+
+
+squares = [x**2 for x in range(10)]
+print(squares)
+
+list1 = [3,4,5]
+
+multiplied = [item*3 for item in list1]
+print(multiplied)
+
+ListOfWords = ["this", "is", "a", "list", "of", "words"]
+result = [word[0].upper() for word in ListOfWords]
+print(result)
+
+#adding an IF condition
+
+    #get the sqaure of every even number from 0-10
+result = [i*i for i in range(11) if i % 2 == 0]
+print(result)
+
+string = "Hello 12345 World"
+numbers = [int(x) for x in string if x.isdigit()]
+print(numbers)
+
+list1 = list(range(1,21))
+print(list1)
+
+list2 = []
+for x in list1:
+    if x%2 == 0:
+        y=x*100
+        list2.append(y)
+print(list2)
+
+list2 = [x*100 for x in list1 if x%2 ==0]
+print(list2)
+
+infile = open('test.txt', 'r')
+result = [i.rstrip('\n') for i in infile if "line3" in i]
+print(result)
+
+def double (x):
+    return x*2
+print(double(10))
+
+result = [double(x) for x in range(10)]
+print(result)
+
+result = [x+y for x in [10,30,50] for y in [20,40,60]]
+print(result)
+
+print('\n\n\n')
+
 
 
 ## Exercise ##
@@ -38,7 +95,8 @@ for i in original_list:
 # which contains only the positive numbers from the list, as integers.
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
-
+newlist = [int(x) for x in numbers if x > 0]
+print(newlist)
 
 
 
@@ -47,7 +105,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
-
+wordlength = [int(len(x)) for x in words if x!= 'the']
+print(wordlength)
 
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
@@ -57,21 +116,27 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
+weight = [x.upper() for x,y in dict.items() if y < 5000]
+print(weight)
+
 
 
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
-
-
+contain = [i for i in range(1,1001) if i % 10 == 4]
+print(contain)
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
-
+infile = open('sometext.txt', 'r')
+total = len([x for x in infile.read().split() if x.lower() == "the"])
+print(total)
 
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
+phrase = f'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
+numbers = [int(x) for x in phrase if x.isdigit()]
+print(numbers)
 
 
 
